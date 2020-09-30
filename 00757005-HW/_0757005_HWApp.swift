3f50be0ddca8718529655318e284e5c93bfc8c56
gitHub <jack940197@gmail.com>
2020-09-30 10:03:37 +0800

    Initial Commit

---
+++ 00757005-HW/_0757005_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0757005_HWApp.swift
+//  00757005-HW
+//
+//  Created by User06 on 2020/9/30.
+//
+
+import SwiftUI
+
+@main
+struct _0757005_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
